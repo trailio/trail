4245a7fd3094994e05d74c210bdeec6d21c6fc93
Jimothy <jimchizhang@gmail.com>
2017-02-24 09:35:30 -0800

    Update changes to auth

--- client/app/components/Auth/LoginSignup.js
+++ client/app/components/Auth/LoginSignup.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, AsyncStorage } from 'react-native';
 import Swiper from 'react-native-swiper';
 
 import styles from '../../styles';
--- client/app/store.js
+++ client/app/store.js
@@ -1,10 +1,32 @@
-import { applyMiddleware, createStore, compose} from 'redux'
+import { applyMiddleware, createStore, compose} from 'redux';
+import createSocketIoMiddleware from 'redux-socket.io';
+import io from 'socket.io-client';
 import ReduxLogger from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import ReduxPromise from 'redux-promise-middleware';
 
 import reducers from "./reducers"
 
+// let socket = io('http://localhost:8000');
+// let socketIoMiddleware = createSocketIoMiddleware(socket, "server/");
+
 const middleware = applyMiddleware(ReduxPromise(), ReduxThunk, ReduxLogger());
 
 export default createStore(reducers, {}, middleware);
+
+
+
+// //
+// function reducer(state = {}, action){
+//   switch(action.type){
+//     case 'message':
+//       return Object.assign({}, {message:action.data});
+//     default:
+//       return state;
+//   }
+// }
+// let store = applyMiddleware(socketIoMiddleware)(createStore)(reducer);
+// store.subscribe(()=>{
+//   console.log('new client state', store.getState());
+// });
+// store.dispatch({type:'server/hello', data:'Hello!'});

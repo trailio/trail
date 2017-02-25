e01c46da4220fbe86cbcd00de38e87a02a5b1147
andrewregal <andrew.regal@gmail.com>
2017-02-25 15:34:13 -0800

    Refactor mysql config for AWS RDS deployment

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,12 @@
 var mysql = require('mysql');
+var config = require('../config.js');
 
 var db_config = {
 	connectionLimit: 10,
-	host: 'localhost',
-	user: 'root',
-	password: '',
+	host: config.AWSRDSHost,
+	port: 3306,
+	user: config.AWSRDSUser,
+	password: config.AWSRDSPassword,
 	database: 'trail'
 }
 

8fa3c36f4e4b7a06883d56aac5679f9ce82a28b2
andrewregal <andrew.regal@gmail.com>
2017-03-01 08:58:35 -0800

    Change all helpers to postgres queries

--- server/db/db.js
+++ server/db/db.js
@@ -1,18 +1,3 @@
-// var mysql = require('mysql');
-// var config = require('../config.js');
-//
-// var db_config = {
-// 	connectionLimit: 10,
-// 	host: config.AWSRDSHost,
-// 	port: 3306,
-// 	user: config.AWSRDSUser,
-// 	password: config.AWSRDSPassword,
-// 	database: 'trail'
-// }
-//
-// var pool = mysql.createPool(db_config);
-// module.exports = pool;
-
 var promise = require('bluebird');
 var options = {
   promiseLib: promise
@@ -31,13 +16,5 @@ var db_config = {
 };
 
 var db = pgp(db_config);
-
-// db.any('select * from posts')
-//   .then(function(data) {
-//     console.log(data);
-//   })
-//   .catch(function(error) {
-//     console.log(error);
-//   });
 
 module.exports = db;
--- server/db/helpers.js
+++ server/db/helpers.js
@@ -5,49 +5,52 @@ module.exports = {
   user: {
     get: function (username, cb) {
       db.any('SELECT * FROM profile WHERE username=$1', username)
-        .then(function(data) {
-          cb(data);
+        .then(function(result) {
+          cb(result);
         })
 				.catch(function(error) {
-					console.log('ERROR: ', error);
-				})
-	  },
-	  signup: function(loginDetails, cb) {
-		  // db.query(`INSERT INTO profile (USERNAME, PASSWORD, EMAIL) VALUES ("${username}", "${password}", "${email}")`, cb);
-			var values = [loginDetails.username, loginDetails.password, loginDetails.email];
-			db.one('INSERT INTO profile(username, password, email) VALUES($1, $2, $3) returning id', values)
-				.then(function(result) {
-					cb(result);
-				})
+          console.log('user.get ERROR: ', error);
+        });
+    },
+    signup: function(loginDetails, cb) {
+      var values = [loginDetails.username, loginDetails.password, loginDetails.email];
+      db.one('INSERT INTO profile(username, password, email) VALUES($1, $2, $3) returning id', values)
+        .then(function(result) {
+          cb(result);
+        })
 				.catch(function(error) {
-					console.log('ERROR: ', error);
+          console.log('user.signup ERROR: ', error);
 				});
-		}
-	},
-	posts: {
-		post: function(longitude, latitude, imageURL, publicPost, cb) {
-			db.query(`INSERT INTO posts (LONGITUDE, LATITUDE, IMAGEURL, PUBLICPOST) VALUES ("${longitude}", "${latitude}", "${imageURL}", "${publicPost}")`, cb);
-		},
-		get: function(cb) {
-			db.query(`SELECT * FROM posts`, cb);
-		},
-		getSentPosts: function(userID, cb) {
-			db.query('SELECT p.recipientUserID as recipientUserID, u.username as recipientUsername, p.longitude as longitude, p.latitude as latitude, p.imageURL as imageURL, p.publicPost as publicPost, p.timePosted as timePosted, p.timeExpired as timeExpired FROM posts p JOIN profile u on p.recipientUserID = u.id WHERE p.userID = $1', userID)
-        .then(function(data) {
-          cb(data);
+    }
+  },
+  posts: {
+    post: function(postDetails, cb) {
+      var values = [postDetails.longitude, postDetails.latitude, postDetails.imageURL, postDetails.publicPost];
+      db.query('INSERT INTO posts(longitude, latitude, imageURL, publicPost) VALUES($1, $2, $3, $4) returning id', values)
+        .then(function(result) {
+          cb(result);
         })
         .catch(function(error) {
-          console.log('ERROR: ', error);
+          console.log('posts.post ERROR: ', error);
         });
-		},
-		getReceivedPosts: function(userID, cb) {
-			db.query('SELECT p.userID as userID, u.username as username, p.longitude as longitude, p.latitude as latitude, p.imageURL as imageURL, p.publicPost as publicPost, p.timePosted as timePosted, p.timeExpired as timeExpired FROM posts p JOIN profile u on p.userID = u.id WHERE p.recipientUserID = $1', userID)
-      .then(function(data) {
-        cb(data);
+    },
+    getSentPosts: function(userID, cb) {
+      db.query('SELECT p.recipientUserID as recipientUserID, u.username as recipientUsername, p.longitude as longitude, p.latitude as latitude, p.imageURL as imageURL, p.publicPost as publicPost, p.timePosted as timePosted, p.timeExpired as timeExpired FROM posts p JOIN profile u on p.recipientUserID = u.id WHERE p.userID = $1', userID)
+        .then(function(result) {
+          cb(result);
+        })
+        .catch(function(error) {
+          console.log('posts.getSentPosts ERROR: ', error);
+        });
+    },
+    getReceivedPosts: function(userID, cb) {
+      db.query('SELECT p.userID as userID, u.username as username, p.longitude as longitude, p.latitude as latitude, p.imageURL as imageURL, p.publicPost as publicPost, p.timePosted as timePosted, p.timeExpired as timeExpired FROM posts p JOIN profile u on p.userID = u.id WHERE p.recipientUserID = $1', userID)
+      .then(function(result) {
+        cb(result);
       })
       .catch(function(error) {
-        console.log('ERROR: ', error);
+        console.log('posts.getReceivedPosts ERROR: ', error);
       });
-		}
-	}
+    }
+  }
 };

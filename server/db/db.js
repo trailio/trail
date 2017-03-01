961eee1331e74ef9fdc6d239d414d8ec59f96245
andrewregal <andrew.regal@gmail.com>
2017-02-28 17:18:52 -0800

    Setup basic postgreSQL connection to RDS

--- server/db/db.js
+++ server/db/db.js
@@ -1,22 +1,43 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
+// var config = require('../config.js');
+//
+// var db_config = {
+// 	connectionLimit: 10,
+// 	host: config.AWSRDSHost,
+// 	port: 3306,
+// 	user: config.AWSRDSUser,
+// 	password: config.AWSRDSPassword,
+// 	database: 'trail'
+// }
+//
+// var pool = mysql.createPool(db_config);
+// module.exports = pool;
+
+var pg = require('pg');
 var config = require('../config.js');
 
-var db_config = {
-	connectionLimit: 10,
-	host: config.AWSRDSHost,
-	port: 3306,
-	user: config.AWSRDSUser,
-	password: config.AWSRDSPassword,
-	database: 'trail'
-}
+var config = {
+  user: config.AWSRDSUser, //env var: PGUSER
+  database: 'trail', //env var: PGDATABASE
+  password: config.AWSRDSPassword, //env var: PGPASSWORD
+  host: config.AWSRDSHost,
+  port: 5432, //env var: PGPORT
+  max: 10,
+  idleTimeoutMillis: 30000
+};
 
-var pool = mysql.createPool(db_config);
-// insert test data
-// pool.query('truncate posts', function(){console.log("truncated posts")
-// 	pool.query(`INSERT INTO posts (userID, longitude, latitude, imageURL, publicPost) VALUES (1, "-122.40743033333337", "37.78728814970013", "https://www.drupal.org/files/project-images/edit(27117).png", TRUE)`, function(){console.log("image1 sent")});
-// 	pool.query(`INSERT INTO posts (userID, recipientUserID, longitude, latitude, imageURL, publicPost) VALUES (1, 2, "-122.40435033333334", "37.78541777072449", "http://www.clipartkid.com/images/43/2014-clipartpanda-com-about-terms-bhiktT-clipart.png", FALSE)`, function(){console.log("image2 sent")});
-// 	pool.query(`INSERT INTO posts (userID, recipientUserID, longitude, latitude, imageURL, publicPost) VALUES (14, 1, "-122.40435033333334", "37.78541777072449", "https://trail-media.s3.amazonaws.com/photos%2Fuser20170227T070834563Z.jpg", FALSE)`, function(){console.log("image3 FALSE")});
-// 	pool.query(`INSERT INTO posts (userID, recipientUserID, longitude, latitude, imageURL, publicPost) VALUES (4, 1, "-122.40630366666669", "37.784332402346976", "https://trail-media.s3.amazonaws.com/photos%2Fphoto.jpg", FALSE)`, function(){console.log("image4 sent")});
-// 	pool.query(`INSERT INTO posts (userID, recipientUserID, longitude, latitude, imageURL, publicPost) VALUES (15, 1, "-122.406417", "37.785834", "https://trail-media.s3.amazonaws.com/photos%2Fuser20170227T072205292Z.jpg", FALSE)`, function(){console.log("image5 sent")});
-// });
+var pool = new pg.Client(config);
+pool.connect();
+
+// example query
+var query = pool.query('SELECT * from posts');
+
+query.on('row', function (row, result) {
+  result.addRow(row);
+});
+query.on('end', function (result) {
+  console.log(JSON.stringify(result.rows, null, '    '));
+  pool.end();
+});
+
 module.exports = pool;

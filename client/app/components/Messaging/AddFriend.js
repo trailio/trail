068abeea97fa021491fcab38aead2a11adf8c8d2
Jimothy <jimchizhang@gmail.com>
2017-03-01 11:03:08 -0800

    Begin render friends on add friend

--- client/app/components/Messaging/AddFriend.js
+++ client/app/components/Messaging/AddFriend.js
@@ -5,6 +5,7 @@ import {
   View,
   TextInput,
   TouchableHighlight,
+  ScrollView,
   Image
 } from 'react-native';
 import Swiper from 'react-native-swiper';
@@ -59,7 +60,33 @@ class AddFriend extends Component {
     //upon return, add a listener to appReducers.js for returned search results in an array, then render them as a list similar to your inbox, with a button click for add friend that will change when clicked
   }
 
+/*
+var that = this
+    var displayUsernames = function() {
+      return (
+        <View>
+        { that.props.receivedPosts.map(function(post, i){
+          return (
+          <TouchableHighlight onPress={function(){console.log('call some function to submit add friends here')}} key={i}>
+            <View style={styles.postBody}>
+              <Image source={pinImg}/>
+              <Text style={styles.postName}> 
+                {post.username}
+              </Text>
+              <Text style={styles.postDate}>
+                {post.timePosted}
+              </Text>
+            </View>
+          </TouchableHighlight>)
+        })}
+        </View>
+      )
+    };
+
+*/
   render () {
+    var that = this
+    var displayUsernames = function() {}
     return (
       <View>
         <View style = {styles.heading}>
@@ -72,6 +99,10 @@ class AddFriend extends Component {
         <TouchableHighlight style={styles.searchButton} onPress={this.onSubmitUsername.bind(this)}>
           <Text style={styles.searchButtonText}>Find Username</Text>
         </TouchableHighlight>
+        <ScrollView bounces={true}>
+          { displayUsernames() }
+        </ScrollView>
+
       </View>)
   }
 }

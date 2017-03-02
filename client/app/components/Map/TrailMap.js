81cc70db5b5a342db8d17b7696a268408644d06e
Jennifer Sha <jennifersha@Jennifers-MacBook-Pro.local>
2017-03-01 23:28:15 -0800

    Create dialog pop up, implement click on marker callout instead of marker

--- client/app/components/Map/TrailMap.js
+++ client/app/components/Map/TrailMap.js
@@ -12,12 +12,13 @@ import ViewContent from './ViewContent';
 import MapView from 'react-native-maps';
 import store from '../../store.js';
 import mapStyle from './mapStyle';
-import Modal from 'react-native-modalbox';
-import Button from 'react-native-button';
+import PopupDialog, { SlideAnimation, DialogButton } from 'react-native-popup-dialog';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as appActions from '../../actions/appActions';
+
+const {height, width} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -52,41 +53,9 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
   },
-    modal: {
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  modal2: {
-    height: 230,
-    backgroundColor: "#3B5998"
-  },
-
-  modal3: {
-    height: 300,
-    width: 300
-  },
-
-  modal4: {
-    height: 300
-  },
-
-  btn: {
-    margin: 10,
-    backgroundColor: "#3B5998",
-    color: "black",
-    padding: 10
-  },
-
-  btnModal: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    width: 50,
-    height: 50,
-    backgroundColor: "transparent"
-  },
-
+  customView: {
+    backgroundColor: 'pink'
+  }
 });
 
 class TrailMap extends Component {
@@ -99,11 +68,7 @@ class TrailMap extends Component {
         longitude: null,
         latitudeDelta: null,
         longitudeDelta: null
-      },
-      isOpen: false,
-      isDisabled: false,
-      swipeToClose: true,
-      sliderValue: 0.3
+      }
     }
   };
 
@@ -118,10 +83,6 @@ class TrailMap extends Component {
     });
   }
 
-  openModal1(id) {
-    this.refs.modal1.open();
-  }
-
   componentWillMount() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -134,11 +95,7 @@ class TrailMap extends Component {
   }
 
   render () {
-
-    console.log('trailmap!!!!!!!!this.props.latitude', this.props.latitude)
-    console.log('trailmap!!!!!!!!store.getState()', store.getState())
-
-    var BContent = <Button onPress={() => this.setState({isOpen: false})} style={[styles.btn, styles.btnModal]}>X</Button>;
+    console.log('this.props.receivedPosts',this.props.receivedPosts)
 
     var image = () => { 
       if (this.props.renderImageURL.length) {
@@ -172,9 +129,35 @@ class TrailMap extends Component {
             >
               <MapView.Marker
                 coordinate={this.state.region}
-                title={"Alfred"}
-                draggable
-              />
+                draggable      
+              >
+                <MapView.Callout onPress={() => {this.popupDialog.openDialog()}} style={styles.customView}>
+                  <View>
+                    <Text>
+                    
+                    </Text>
+                  </View>
+
+                </MapView.Callout>
+              </MapView.Marker>
+              <PopupDialog 
+                ref={(popupDialog) => {this.popupDialog=popupDialog}} 
+                height={height} 
+                dialogAnimation={new SlideAnimation({slideFrom: 'top'})} 
+                actions={[
+                  <DialogButton
+                    text='X'
+                    onPress={() => {this.popupDialog.dismiss()}}
+                    key='button-1'
+                    align='left'
+                  />
+                ]}>
+                <View>
+                  {image()}
+                </View>
+              </PopupDialog>
+
+
             {this.props.receivedPosts.map((marker,i) => (
               <MapView.Marker
                 coordinate={{
@@ -189,18 +172,6 @@ class TrailMap extends Component {
               </MapView.Marker>
             ))}
             </MapView> : null}
-            <View>
-              <Modal
-                style={[styles.modal, styles.modal1]}
-                ref={"modal1"}
-                swipeToClose={this.state.swipeToClose}
-                onClosed={this.onClose}
-                onOpened={this.onOpen}
-                onClosingState={this.onClosingState}>
-                  <Text>Hi</Text>
-                  <Button onPress={() => this.setState({swipeToClose: !this.state.swipeToClose})} style={styles.btn}>Disable swipeToClose({this.state.swipeToClose ? "true" : "false"})</Button>
-              </Modal>
-            </View>
         </View>
         <View style={styles.slide1}>
           <ViewContent />

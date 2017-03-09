79a5324808f9ed3348cd98b50cee3dcfb6c63942
andrewregal <andrew.regal@gmail.com>
2017-03-08 23:42:37 -0800

    Modify redux store to connect socket with EBS

--- client/app/store.js
+++ client/app/store.js
@@ -1,13 +1,14 @@
 import { applyMiddleware, createStore, compose} from 'redux';
 import createSocketIoMiddleware from 'redux-socket.io';
 import io from 'socket.io-client';
+
 import ReduxThunk from 'redux-thunk';
 import ReduxPromise from 'redux-promise-middleware';
-
 import reducers from './reducers';
 
-// let socket = io('http://localhost:8000');
-let socket = io('http://5cc669df.ngrok.io');
+let socket = io('http://trail-server.us-west-2.elasticbeanstalk.com/');
+// let socket = io('http://localhost:49160');
+// let socket = io('http://5cc669df.ngrok.io');
 
 let socketIoMiddleware = createSocketIoMiddleware(socket, 'socket/');
 
